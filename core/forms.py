0c4f68a710807e8ab232f408acb6eb934c21d05d
Gadeonio <95529947+Gadeonio@users.noreply.github.com>
2022-03-21 16:16:05 +0500

    #4 Создана форма с добавлением, удалением и изменением книг

--- core/forms.py
+++ core/forms.py
@@ -5,10 +5,16 @@ import core.models
 
 class BookSearch(forms.Form):
     name = forms.CharField(label='Название', required=False)
-    min_pages = forms.IntegerField(label='Кол-во страниц', required=False, help_text='Минимальное количество страниц')
+    pages = forms.IntegerField(label='Кол-во страниц', required=False, help_text='Минимальное количество страниц')
+    author = forms.ModelChoiceField(label='Автор', queryset=core.models.Author.objects.all(), to_field_name='name')
 
     def clean(self):
-        raise forms.ValidationError('Ошибка')
+        pages = self.cleaned_data.get('pages')
+
+        if pages and pages > 1000:
+            raise forms.ValidationError('Количество страниц не может быть больше 1000')
+
+        return pages
 
 
 
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.decorators.http import require_http_methods
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView, DeleteView
 
 import core.models
 import core.forms
@@ -40,15 +41,17 @@ class Books(TitleMixin, ListView):
         return core.filters.BookFilter(self.request.GET)
 
     def get_queryset(self):
-        name = self.request.GET.get('name')
-        queryset = core.models.Book.objects.all()
-        if name:
-            queryset = queryset.filter(name__icontains=name)
-        return queryset
+        # name = self.request.GET.get('name')
+        # queryset = core.models.Book.objects.all()
+        # if name:
+        #     queryset = queryset.filter(name__icontains=name)
+        # return queryset
+        return self.get_filters().qs
 
     def get_context_data(self):
         context = super().get_context_data()
         context['form'] = core.forms.BookSearch(self.request.GET or None)
+        #context['filters'] = self.get_filters()
         return context
 
 
@@ -58,5 +61,35 @@ class BookDetail(TitleMixin, DetailView):
     def get_title(self):
         return str(self.get_object())
 
-# Create your views here.
+
+class BookUpdate(TitleMixin, UpdateView):
+    model = core.models.Book
+    form_class = core.forms.BookEdit
+
+    def get_title(self):
+        return f'Изменение данных книги "{str(self.get_object())}"'
+
+    def get_success_url(self):
+        return reverse('core:book_list')
+
+
+class BookCreate(TitleMixin, CreateView):
+    model = core.models.Book
+    form_class = core.forms.BookEdit
+    title = 'Добавление книги'
+
+    def get_success_url(self):
+        return reverse('core:book_list')
+
+
+class BookDelete(TitleMixin, DeleteView):
+    model = core.models.Book
+
+    def get_title(self):
+        return f'Удаление книги {str(self.get_object())}'
+
+    def get_success_url(self):
+        return reverse('core:book_list')
+    #14:10
+    # Create your views here.
 
